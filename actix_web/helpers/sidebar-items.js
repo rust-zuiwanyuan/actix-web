3760792409fda77e424e26604b14e53459528d82
Travis CI User <travis@example.org>
2018-03-30 02:44:22 +0000

    Documentation upload

---
+++ actix_web/helpers/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["NormalizePath","Path normalization helper"]]});
---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/header/enum.ConnectionType.html\" title=\"enum actix_web::header::ConnectionType\">ConnectionType</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/ws/enum.OpCode.html\" title=\"enum actix_web::ws::OpCode\">OpCode</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/ws/enum.CloseCode.html\" title=\"enum actix_web::ws::CloseCode\">CloseCode</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/header/enum.ContentEncoding.html\" title=\"enum actix_web::header::ContentEncoding\">ContentEncoding</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"actix_web/httpcodes/struct.StaticResponse.html\" title=\"struct actix_web::httpcodes::StaticResponse\">StaticResponse</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/server/enum.KeepAlive.html\" title=\"enum actix_web::server::KeepAlive\">KeepAlive</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
